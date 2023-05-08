6f65665857e42ce8ac53e3898b9b8281284b5d1c
HemaMurugesan2794 <89142030+HemaMurugesan2794@users.noreply.github.com>
2023-05-08 20:42:09 +0530

    Add files via upload

---
+++ Week3/Day 1/MaximumAverage.java
@@ -0,0 +1,53 @@
+package week3.day2;
+
+import org.junit.Test;
+
+public class MaximumAverage {
+	
+	//Time Complexity: O(n)
+	
+	
+		@Test
+		public void example()
+		{
+			int[] arr= {1,3,5,6,4};
+			int k=3;
+			double maxsum=maxsum(arr,k) ;
+			System.out.println(maxsum);
+		}
+		
+		@Test
+		public void example1()
+		{
+			int[] arr= {4};
+			int k=1;
+			double maxsum=maxsum(arr,k) ;
+			System.out.println(maxsum);
+		}
+
+		public static double  maxsum(int[] arr,int k) 
+		{
+			int maxSum=0;
+			int sum=0;
+			for (int i = 0; i < k; i++) 
+			{
+				sum = sum+arr[i];
+			}
+			//System.out.println(sum);
+			maxSum=sum;
+			for (int i = k; i < arr.length; i++)
+			{
+				
+				sum = sum+arr[i]- arr[i - k];
+				maxSum = Math.max(maxSum, sum);
+				
+			}
+			
+			return maxSum/2;
+			
+		}
+
+	}
+
+
+
